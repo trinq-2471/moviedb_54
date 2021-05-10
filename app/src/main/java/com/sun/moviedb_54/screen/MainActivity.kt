c910e7776e898f0a7cc88fab24bf30782a606be1
trinq-2471 <nguyen.quang.tri@sun-asterisk.com>
2021-05-10 22:40:19 +0700

    init_base

---
+++ app/src/main/java/com/sun/moviedb_54/screen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sun.moviedb_54.screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sun.moviedb_54.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
